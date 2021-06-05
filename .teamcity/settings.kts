11b2001544cd882bccfcb91f2233e1bcceb3a2b0
maryzam <zamo.code@gmail.com>
2021-06-05 15:18:17 +1200

    Update deployment & build scripts location

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -4,36 +4,13 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.nodeJS
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
 import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
 
-/*
-The settings script is an entry point for defining a TeamCity
-project hierarchy. The script should contain a single call to the
-project() function with a Project instance or an init function as
-an argument.
-
-VcsRoots, BuildTypes, Templates, and subprojects can be
-registered inside the project using the vcsRoot(), buildType(),
-template(), and subProject() methods respectively.
-
-To debug settings scripts in command-line, run the
-
-    mvnDebug org.jetbrains.teamcity:teamcity-configs-maven-plugin:generate
-
-command and attach your debugger to the port 8000.
-
-To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
--> Tool Windows -> Maven Projects), find the generate task node
-(Plugins -> teamcity-configs -> teamcity-configs:generate), the
-'Debug' option is available in the context menu for the task.
-*/
-
 version = "2021.1"
 
 project {
-
-    buildType(Build)
+    buildType(BuildInstagramPost)
 }
 
-object Build : BuildType({
+object BuildInstagramPost : BuildType({
     name = "Build Instagram Post"
 
     artifactRules = "+:releases/%build.number% => insagram-post-%build.number%.zip"
@@ -54,20 +31,18 @@ object Build : BuildType({
             name = "Install npm packages"
             scriptContent = "npm install"
         }
+
         script {
             name = "Run tests"
             scriptContent = "npm run test"
         }
+
         script {
             name = "Run Instagram post"
             scriptContent = """
                 node --version
-                node ./build/index.js --publish=./releases/%build.number% --source=./src/post.json
+                node ./.build/index.js --publish=./releases/%build.number% --source=./src/post.json
             """.trimIndent()
-        }
-        nodeJS {
-            enabled = false
-            shellScript = "npm run test"
         }
     }
 
